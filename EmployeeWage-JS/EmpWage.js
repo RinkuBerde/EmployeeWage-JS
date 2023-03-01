c82aa86f4190261ec0fabf60030f36bd97531f79
rinkuberde <rinkuberde@gmail.com>
2023-03-01 14:32:40 +0530

    Calculate Daily Employee Wage

--- EmployeeWage-JS/EmpWage.js
+++ EmployeeWage-JS/EmpWage.js
@@ -1,10 +1,24 @@
-console.log("Welcome To Employee wage Program Using JavaScript");
-//UC-01----->Ability to Check Employee is present or Absent
-const IS_Absent = 0
-let empCheck = Math.floor(Math.random() * 10) % 2;
-if (empCheck == IS_Absent) {
-    console.log("Employee is Absent");
+//UC-02------->Ability to Calculate Daily Employee Wage
+const IS_PART_TIME=1;
+const IS_FULL_TIME=2;
+const PART_TIME_HOURS=4;
+const FULL_TIME_HOURS=8;
+const WAGE_PER_HOUR=20;
+
+let empHrs=0;
+let empCheck=Math.floor(Math.random()* 10 % 3);
+switch(empCheck)
+{
+ case IS_PART_TIME:
+      //employee is present and part-time
+      empHrs=PART_TIME_HOURS;
+      break;
+case IS_FULL_TIME:
+    //employee is present and full time
+      empHrs=FULL_TIME_HOURS;
+      break;
+default:
+      empHrs=0;
 }
-else {
-    console.log("Employee is Present");
-}
+let empWage = empHrs * WAGE_PER_HOUR;
+console.log("Emp wage: "+empWage);
