758df93433f65cfabc9b08ff41c34fe7726f42ea
rinkuberde <rinkuberde@gmail.com>
2023-03-01 14:01:27 +0530

    Ability to Check Employee is present or Absent

--- EmployeeWage-JS/EmpWage.js
+++ EmployeeWage-JS/EmpWage.js
@@ -1,1 +1,10 @@
 console.log("Welcome To Employee wage Program Using JavaScript");
+//UC-01----->Ability to Check Employee is present or Absent
+const IS_Absent = 0
+let empCheck = Math.floor(Math.random() * 10) % 2;
+if (empCheck == IS_Absent) {
+    console.log("Employee is Absent");
+}
+else {
+    console.log("Employee is Present");
+}
