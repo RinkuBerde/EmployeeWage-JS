7ce8747ec19bddad3f72b733bd368d3755f97107
rinkuberde <rinkuberde@gmail.com>
2023-03-09 21:50:59 +0530

    create Employee Payroll Data with id, name and salary

--- EmployeeWage-JS/EmpWage.js
+++ EmployeeWage-JS/EmpWage.js
@@ -294,5 +294,30 @@ while (totalEmpHrs <= MAX_HRS_IN_MONTH &&
                                  .map(dailyHrsAndWage => dailyHrsAndWage.dayNum);
       console.log("\nUC-11D nonWorkingDayNums: "+ nonWorkingDayNums);
       
+      
+      //UC-12--->Create Employee Payroll Data with id, name and salary
+      class EmployeePayrollData{
+        //property
+        id;
+        salary;
 
+        //constructor
+        constructor(id, name, salary){
+          this.id=id;
+          this.name=name;
+          this.salary=salary;
+        }
+        //getter and setter method
+        get name(){ return this._name; }
+        set name(name) {this._name = name; }
 
+        //method
+        toString(){
+          return "id=" + this.id + ", name=" + this.name +", salary=" +this.salary;
+        }
+      }
+      let employeePayrollData = new EmployeePayrollData(1, "Mark", 30000);
+      console.log(employeePayrollData.toString());
+      employeePayrollData.name = "john";
+      console.log(employeePayrollData.toString());
+      
